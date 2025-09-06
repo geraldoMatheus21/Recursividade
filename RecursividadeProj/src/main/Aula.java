29b6e9d7a789092ae05373ea3ceb0114b58db410
geraldoMatheus21 <geraldomatheus2@gmail.com>
2025-09-05 23:53:14 -0300

    Sem exercicios

---
+++ RecursividadeProj/src/main/Aula.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Aula {
+
+	public static void main(String[] args) {
+		
+		System.out.println(fatorial(5));
+	}
+	
+	public static int fatorial(int num) {
+		if(num==0) {
+			return 1;
+		}
+		return num* fatorial(num-1);
+	}
+
+}
